d914f4f009c30489c8be20afc9df8839dd5f5064
Nischith <125783753+nischithnayak@users.noreply.github.com>
2025-04-03 15:25:14 +0530

    new

---
+++ JSDOM/app.js
@@ -0,0 +1,5 @@
+let smallImages=document.getElementsByClassName("oldImg");
+for(let i=0;i<smallImages.length;i++){
+    smallImages[i].src="assets/spiderman_img.png";
+    console.log(`value of image no. ${i} is changed.`);
+}
