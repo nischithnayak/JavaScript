8993844c4cabe96ac8448577c5e9f81dbf1daf23
Nischith <125783753+nischithnayak@users.noreply.github.com>
2025-06-19 11:24:04 +0530

    new

--- JS-11/app.js
+++ JS-11/app.js
@@ -1,20 +1,32 @@
 let h1 = document.querySelector("h1");
 
-function changeColor(color, delay, nextColorChange) {
-    setTimeout(() => {
-        h1.style.color = color;
-        if (nextColorChange) nextColorChange();
-    }, delay);
+function changeColor(color, delay) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            h1.style.color = color;
+            resolve(`${color} color changed!`);
+        }, delay);
+    });
 }
 
-changeColor("red", 1000, () => {
-    changeColor("orange", 1000, () => {
-        changeColor("yellow", 1000, () => {
-            changeColor("green", 1000, () => {
-                changeColor("blue", 1000, () => {
-                    console.log("Color changes complete!");
-                });
-            });
-        });
+changeColor("red", 1000)
+    .then(() => {
+        console.log("Red color was completed");
+        return changeColor("orange", 1000);
+    })
+    .then(() => {
+        console.log("Orange color was completed");
+        return changeColor("yellow", 1000);
+    })
+    .then(() => {
+        console.log("Yellow color was completed");
+        return changeColor("green", 1000);
+    })
+    .then(() => {
+        console.log("Green color was completed");
+        return changeColor("blue", 1000);
+    })
+    .then(() => {
+        console.log("Blue color was completed. Color changes complete!");
     });
-});
+
