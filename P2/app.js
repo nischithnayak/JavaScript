30c0ad28a109b5db93db53d259c904f824d3175b
Nischith <125783753+nischithnayak@users.noreply.github.com>
2025-08-20 12:33:08 +0530

    new

---
+++ P2/app.js
@@ -0,0 +1,13 @@
+let color="white";
+switch(color){
+    case 'red':
+        console.log("Red");
+        break;
+    case 'yellow':
+        console.log("yellow");
+        break;
+    case 'green':
+        break;
+    default: console.log("Not Match");
+    break;
+}
---
+++ P3/app.js
@@ -0,0 +1,6 @@
+let str=prompt("Enter string:");
+if(str.length>3 && str.charAt(0)==='a'){
+    console.log("Good String");
+}else{
+    console.log("Not a Good String");
+}
