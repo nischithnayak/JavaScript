61c2b019397ec403ea41cda166bba0faff539015
Nischith <nischithnayak6@gmail.com>
2026-05-18 09:18:51 +0530

    Added practiced files

---
+++ P19/app.js
@@ -0,0 +1,22 @@
+// function savetoDb(data) {
+//     return new Promise((resolve, reject) => {  
+//         let internetSpeed = Math.floor(Math.random() * 10) + 1;
+//         if (internetSpeed > 4) {
+//             resolve("Success: data was saved");
+//         } else {
+//             reject("Failure: weak connection");
+//         }
+//     });
+// }
+// let request =saveToDBPromise("apna College");
+// request
+//     .then(()=>{
+//         console.log("promise resolved");
+//     })
+//     .catch(()=>{
+//         console.log("promise rejected");
+//     });
+
+
+
+
