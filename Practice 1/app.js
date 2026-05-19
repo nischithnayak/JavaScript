a8724f506472285bb74fc4dd43ffc7c49a671cb6
Nischith <nischithnayak6@gmail.com>
2026-05-19 09:50:35 +0530

    Added JS basic code

--- Practice 1/app.js
+++ Practice 1/app.js
@@ -1,3 +1,15 @@
 let age=23;
 age +=1;
 console.log(`${age}`);
+
+let name="Nischith Nayak";
+console.log(`Length: ${name.length}`);
+console.log(`${name[0]}`);
+console.log(`${name[name.length-1]}`);
+console.log(`${"Nischith" +123}`);
+let empty="";
+console.log(`${empty.length}`);
+console.log("Apna College");
+console.log(1234);
+console.log(2+2);
+console.log("Apna", "College", 123);
