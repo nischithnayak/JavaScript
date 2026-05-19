114a5334829d5360b90de665aa1c5f430c778be0
Nischith <nischithnayak6@gmail.com>
2026-05-19 10:02:59 +0530

    Added arithmatic operator

---
+++ Practice 3/app.js
@@ -0,0 +1,14 @@
+let a=5;
+let b=10;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a&&b);
+console.log(a%b);
+console.log(a**b);
+console.log(a++);
+console.log(++a);
+
+
+
