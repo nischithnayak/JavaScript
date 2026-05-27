a472dbea4b966b8678a6482736e480eafb8032a2
Nischith <nischithnayak6@gmail.com>
2026-05-27 10:19:02 +0530

    Added some examples

---
+++ Practice 5/app8.js
@@ -0,0 +1,6 @@
+let str="Adjkdfsdf";
+if(str.length>5 && str[0]=='a' || str[0]=='A'){
+    console.log("Golden String");
+} else{
+    console.log("Not a Golden String");
+}
---
+++ Practice 5/app9.js
@@ -0,0 +1,3 @@
+let a=5, b=9, c=34;
+let largest=(a>b)? (a>c? a:c) : (b>c ? b:c);
+console.log(`${largest} is the largest` );
