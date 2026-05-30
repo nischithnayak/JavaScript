f151018c756673043daf1026e82fcf0a5e9a730b
Nischith <nischithnayak6@gmail.com>
2026-05-30 21:04:48 +0530

    Added

--- Practice 5/app12.js
+++ Practice 5/app12.js
@@ -6,3 +6,21 @@ console.log(`${msg.trim().toUpperCase()}`);
 
 let name="Apna College";
 console.log(`${name.slice(4).replace('l','t').replace('t','l')}`);
+
+let students=["aman", "Nischith", "Shradha"];
+console.log(`${students}`);
+console.log(`${students.length}`);
+
+let marks=[99,85,93,76,62];
+let names=["adam", "bob", "Catlyn"];
+let info=["aman", 25, 6.1];
+let newArr=[];
+
+let fruits=["mango","apple","Litchi"];
+fruits[0]="bananna";
+console.log(fruits);
+
+let name1="rohit";
+name1[0]="m";
+console.log(name1);
+
