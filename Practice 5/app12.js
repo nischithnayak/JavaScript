38cfacfe66e9634a2eab3e636aed0bd2cc4aa44b
Nischith <nischithnayak6@gmail.com>
2026-05-30 18:17:26 +0530

    Added some more changes

---
+++ Practice 5/app12.js
@@ -0,0 +1,8 @@
+let str="Mango";
+console.log(`${str.repeat(3)}`);
+
+let msg="help!";
+console.log(`${msg.trim().toUpperCase()}`);
+
+let name="Apna College";
+console.log(`${name.slice(4).replace('l','t').replace('t','l')}`);
