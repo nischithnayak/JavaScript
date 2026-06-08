7c7e23a5c5238a3581f89b9efd455fb8ab3e1ded
Nischith <nischithnayak6@gmail.com>
2026-06-08 09:59:09 +0530

    Added

---
+++ Practice 13/app.js
@@ -0,0 +1,26 @@
+// let str=prompt("please enter a string");
+// if(str.length==0){
+//     console.log("String is empty");
+// }else{
+//     console.log("String is not empty");
+// }
+
+// let str="ApNaCollEge";
+// let idx=3;
+// if(str[idx]==str[idx].toLowerCase()){
+//     console.log("Character is lowercase");
+// }else{
+//     console.log("Character is not lowercase");
+// }
+
+// let str=prompt("Please enter a string");
+// console.log(`Original String=${str}`);
+// console.log(`String without spaces =${str.trim()}`);
+
+let arr=["hello", 'a', 23, 64,99, -6];
+let item=64;
+if(arr.indexOf(item)!=-6){
+    console.log("Elements exists in array");
+}else{
+    console.log("Element doesnt exist in array");
+}
