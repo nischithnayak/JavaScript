e1ac4127c8e849df1456ed83ab1dc83dbfe46809
Nischith <nischithnayak6@gmail.com>
2026-06-08 09:46:52 +0530

    Added

---
+++ Practice 12/app.js
@@ -0,0 +1,10 @@
+let nums=[[2,4],[3,6],[4,8]];
+console.log(nums[0].length);
+let str=[['x','','o'],['','x',''],['o','','x']];
+console.log(str);
+
+let arr=[7,9,0,-2];
+let n=3;
+let ans=arr.slice(0,n);
+let ans1=arr.slice(arr.length-n);
+console.log(ans1);
