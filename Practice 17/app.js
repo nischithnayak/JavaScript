8ce96afa9468c5464652bc24d6f6c2c3cd361540
Nischith <nischithnayak6@gmail.com>
2026-06-18 10:43:43 +0530

    Added Object of Objects

---
+++ Practice 17/app.js
@@ -0,0 +1,16 @@
+const classInfo={
+    aman:{
+        grade: "A+",
+        city: "Delhi",
+    },
+    Shradha:{
+        grade: "B+",
+        city:"Mumbai",
+    },
+    karan:{
+        grade:"C+",
+        city:"Banglore",
+    }
+
+
+}
