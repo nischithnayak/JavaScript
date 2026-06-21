943dc5d9b9e37114e70c4b19ddb109520be8a839
Nischith <nischithnayak6@gmail.com>
2026-06-21 21:21:41 +0530

    Added Some more example of objects

---
+++ Practice 21/app.js
@@ -0,0 +1,12 @@
+const car={
+    name:"Maruthi suzuki",
+    model:"Dezire",
+    color:"White"
+};
+console.log(car.name);
+
+const Person ={
+    name:"Jane Doe",
+    age:21,
+    city:"New Jersey",
+}
