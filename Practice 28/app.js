d27ba356730d102758e07998950451486893e12c
Nischith <nischithnayak6@gmail.com>
2026-07-05 19:29:16 +0530

    Added Query Selector

---
+++ Practice 28/app.js
@@ -0,0 +1,9 @@
+let btn=document.querySelector("button");
+let p=document.querySelector("p");
+let h1=document.querySelector("h1");
+let h3=document.querySelector("h3");
+
+function changeColor(){
+    console.dir(this.innerText);
+    this.style.backgroundColor="blue";
+}
